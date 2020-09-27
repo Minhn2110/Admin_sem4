2786cdd06d12b86d3591f90aa06a421df8514a70
Minh Nguyen <minhnha@smartosc.com>
2020-09-27 21:02:57 +0700

    deploy test 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+
 export const environment = {
-	production: true,
-	isMockEnabled: true, // You have to switch this, when your real back-end is done
-	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
+	production: false,
+	isMockEnabled: false, // You have to switch this, when your real back-end is done
+	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
+	api_url: 'http://www.genson.me/api/v1'
 };
